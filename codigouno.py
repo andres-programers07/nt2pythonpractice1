cf50fa7e951c685f00d5bd4f29a4ebab62b73b11
andres mosquera <ormoca2009@hotmail.com>
2023-08-11 11:11:38 -0500

    nt2 se declara nueva variable de cantidad de hijos

--- codigouno.py
+++ codigouno.py
@@ -1,3 +1,4 @@
 nombreusuario = input("ingrese el nombre")
 direccionusuario = input("ingrese direccion")
 edad = int(input("ingrese la edad"))
+hijosusuario =int(input("ingrese cantidad de hijos"))
